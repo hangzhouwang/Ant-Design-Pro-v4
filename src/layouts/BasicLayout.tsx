c0d569287b0855728f8d79191f18506e92feaf80
siaoynli <siaoynli@126.com>
2020-07-29 15:02:10 +0800

    fix bug

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -23,10 +23,10 @@ const noMatch = (
   <Result
     status={403}
     title="403"
-    subTitle="Sorry, you are not authorized to access this page."
+    subTitle="对不起，您没有权限访问此页面."
     extra={
       <Button type="primary">
-        <Link to="/user/login">Go Login</Link>
+        <Link to="/user/login">登 录</Link>
       </Button>
     }
   />
